ba4a3fc8c339b72c05337dd63b44870b26513089
shvartserm <shvartser.m@yandex.ru>
2025-04-27 13:17:54 +0300

    add new py

--- collect_files.py
+++ collect_files.py
@@ -3,34 +3,41 @@ import shutil
 import argparse
 
 
-def copy_file(filepath: str, output_dir: str):
+def gen_filename(output_dir: str, relative_path: list[str], filename: str):
+    if not os.path.exists(os.path.join(output_dir, *relative_path, filename)):
+        return filename
+    
+    suffix = 1
+    base, extension = os.path.splitext(filename)
+    while True:
+        new_filename = f"{base}{suffix}{extension}"
+        if os.path.exists(os.path.join(output_dir, *relative_path, new_filename)):
+            suffix += 1
+            continue
+        else:
+            return new_filename
+
+
+def copy_file(filepath: str, relative_path: list[str], output_dir: str, max_depth: int):
     filename = os.path.basename(filepath)
-    if not os.path.exists(os.path.join(output_dir, filename)):
-        shutil.copy(filepath, output_dir)
-        return
+
+    if max_depth in [-1, 1]:
+        relative_path = []
     else:
-        suffix = 1
-        base, extension = os.path.splitext(filename)
-        while True:
-            new_filename = f"{base}{suffix}{extension}"
-            if os.path.exists(os.path.join(output_dir, new_filename)):
-                suffix += 1
-                continue
-            else:
-                shutil.copy(filepath, os.path.join(output_dir, new_filename))
-                break
+        relative_path = relative_path[-(max_depth-1):]
+    
+    os.makedirs(os.path.join(output_dir, *relative_path), exist_ok=True)
+    filename = gen_filename(output_dir, relative_path, filename)
+    shutil.copy(filepath, os.path.join(output_dir, *relative_path, filename))
 
 
-def collect_files(input_dir: str, output_dir: str, depth: int = -1):
+def collect_files(input_dir: str, output_dir: str, relative_path: list[str] = [], max_depth: int = -1):
     for p in os.listdir(input_dir):
         path = os.path.join(input_dir, p)
         if os.path.isfile(path):
-            copy_file(path, output_dir)
+            copy_file(path, relative_path, output_dir, max_depth)
         elif os.path.isdir(path):
-            if depth == -1:
-                collect_files(path, output_dir)
-            elif depth > 1:
-                collect_files(path, output_dir, depth-1)
+            collect_files(path, output_dir, relative_path + [os.path.basename(path)], max_depth)
 
 
 def main():
@@ -49,7 +56,7 @@ def main():
         print("ERROR: output_dir must be an existing directory")
         return
 
-    collect_files(args.input_dir, args.output_dir, args.max_depth)
+    collect_files(args.input_dir, args.output_dir, [], args.max_depth)
 
 
 if __name__ == "__main__":
